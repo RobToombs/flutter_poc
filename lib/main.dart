23cfe1ca52ce56ffae01212511b3b09b76a4500f
Rob Toombs <toombs.rob@gmail.com>
2021-02-17 16:59:57 -0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'sign_up/sign_up.dart';
+
+void main() => runApp(SignUpApp());
+
+class SignUpApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => SignUpScreen(),
+      },
+    );
+  }
+}
